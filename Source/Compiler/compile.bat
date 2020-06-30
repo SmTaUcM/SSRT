37b33e5784c40712b00602cc7f0facf6e4f08703
Unknown <SmTaUcM@Yahoo.co.uk>
2020-06-30 13:06:42 +0100

    Feature - Compiler upgraded to do housekeeping.

--- Source/Compiler/compile.bat
+++ Source/Compiler/compile.bat
@@ -1,1 +1,5 @@
+rmdir "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist" /S /Q
 python compile.py py2exe --includes sip
+mkdir "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist\Resource"
+xcopy "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\Resource" "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist\Resource\" /E
+rmdir "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\build" /S /Q
