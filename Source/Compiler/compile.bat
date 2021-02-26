96ed5513adda393cf741304ea795cc686372efa7
Stuart Macintosh <SmTaUcM@Yahoo.co.uk>
2021-02-26 10:22:50 +0000

    v1.03 Alpha

--- Source/Compiler/compile.bat
+++ Source/Compiler/compile.bat
@@ -1,4 +1,4 @@
-rmdir "C:\Users\smtau\Dropbox\TIECorps\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist" /S /Q
+rmdir "D:\Stuart\Downloads\TIECorps\Tools\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist" /S /Q
 python compile.py py2exe --includes sip
-copy "C:\Users\smtau\Dropbox\TIECorps\SkyShadow's Rapid Reporting Tool\Source\Resource\srrt.ui" "C:\Users\smtau\Desktop\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist\" 
-rmdir "C:\Users\smtau\Dropbox\TIECorps\SkyShadow's Rapid Reporting Tool\Source\Compiler\build" /S /Q
+copy "D:\Stuart\Downloads\TIECorps\Tools\SkyShadow's Rapid Reporting Tool\Source\Resource\srrt.ui" "D:\Stuart\Downloads\TIECorps\Tools\SkyShadow's Rapid Reporting Tool\Source\Compiler\dist\" 
+rmdir "D:\Stuart\Downloads\TIECorps\Tools\SkyShadow's Rapid Reporting Tool\Source\Compiler\build" /S /Q
